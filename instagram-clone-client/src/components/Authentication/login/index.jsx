d746b05559b0b951427ba64c2f17ef30467d36f2
unknown <youssefhoussein701@gmail.com>
2023-08-10 04:12:16 +0300

    client <> handle the login with axios

--- instagram-clone-client/src/components/Authentication/login/index.jsx
+++ instagram-clone-client/src/components/Authentication/login/index.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css';
 import '../../../utilities.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 
 const Login = () => {
+    const [data , setData] = useState({
+        email: "",
+        password : "",
+    })
+    const handleDataChange = (e) => {
+        setData({...data, [e.target.name]: e.target.value});
+    }
+    console.log("hiieea")
+    const handleLogin = async () => {
+        const response = await axios.post('http://127.0.0.1:8000/api/login',data);
+        console.log("hi")
+        console.log(response.data)
+    }
+   
+    
   return (
     <div className='container flex col'>
         <div className='login-container flex col '>
             <div className="title"><i className='logo'></i></div>
             <div className="inputs flex col">
                 <div className="text flex col">
-                    <input type="text" placeholder='email'/>
-                    <input type="password" name="" id="" placeholder='password'/>
+                    <input type="text" name='email' placeholder='email'  onChange={handleDataChange}/>
+                    <input type="password" name="password" id="" placeholder='password' onChange={handleDataChange}/>
                 </div>
                 <div className="button flex center">
-                <Link to={"/dashbaord"} className='link flex center'><button className='login-button'>Log in</button></Link>
+                <Link to={"/dashbaord"} className='link flex center'><button className='login-button' onClick={handleLogin}>Log in</button></Link>
                 </div>
             </div>
         </div>
