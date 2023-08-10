bc582b9bdc4729101c7716e20c99bde5c3b2fa68
unknown <youssefhoussein701@gmail.com>
2023-08-10 23:29:33 +0300

    client <> handle the follow

--- instagram-clone-client/src/components/Dashboard/searchCard/index.jsx
+++ instagram-clone-client/src/components/Dashboard/searchCard/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './index.css'
 const SearchCard = ({name , follow}) => {
+  const handleFollow = async () => {
+    const response = await axios.post('http://127.0.0.1:8000/api/follow', data,{
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+          
+        },
+      });
+  }
   return (
     <div className='search flex'>
         <span>{name}</span>
-        <span>{follow}</span>
+        <span onClick={handleFollow}>{follow}</span>
     </div>
   )
 }
