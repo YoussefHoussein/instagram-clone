a9505cb38649c7ffb4d3c31a63904437ab4c1f37
unknown <youssefhoussein701@gmail.com>
2023-08-10 05:17:27 +0300

    server <> add the models needed

---
+++ instagram-clone-server/app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Following;
+use App\Models\User;
+use App\Models\Like;
+
+class PostController extends Controller
+{
+    //
+    public function getPosts(Request $request){
+        
+    }
+}
